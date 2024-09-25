c0f4dc11796070605a9665e6a9e5f75d7c412a62
luizgustavojunqueira <luizgustavossj@gmail.com>
2024-09-25 10:59:16 -0400

    feat: run dblp dataset

--- graph_script.sh
+++ graph_script.sh
@@ -61,3 +61,31 @@ do
       ./main ./graph_datasets/ca_astroph.edgelist $k $alg >> cliques.results
     done
 done
+
+echo ""
+
+echo "Executando o código para o dataset dblp"
+
+echo "dblp" >> cliques.results
+# Executar o código para o dataset dblp para os valores de k = 3, 4, 5 e 6 e alg = 1, 2 e 3
+for alg in 1 2
+do
+
+  if [ $alg -eq 1 ];
+  then
+    echo "Executando o algoritmo sequencial"
+  else
+    if [ $alg -eq 2 ];
+    then
+      echo "Executando o algoritmo paralelo"
+    else 
+      echo "Executando o algoritmo paralelo com balanceamento de carga"
+    fi
+  fi
+
+  for k in 3 4 5 6
+    do
+      echo "Executando para k = $k"
+      ./main ./graph_datasets/dblp.edgelist $k $alg >> cliques.results
+    done
+done

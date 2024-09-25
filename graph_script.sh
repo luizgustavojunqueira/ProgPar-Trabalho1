f3c78059436288457f3d2d195c732ba98d0ac646
luizgustavojunqueira <luizgustavossj@gmail.com>
2024-09-25 10:35:55 -0400

    refactor: bash script executes in a for loop

--- graph_script.sh
+++ graph_script.sh
@@ -3,10 +3,12 @@
 #compilar o código 
 make main
 
-#
-./main ./graph_datasets/citeseer.edgelist 3 1 >>citeseer.results
-./main ./graph_datasets/citeseer.edgelist 4 1 >>citeseer.results
-./main ./graph_datasets/citeseer.edgelist 5 1 >>citeseer.results
-./main ./graph_datasets/citeseer.edgelist 6 1 >>citeseer.results
-
+# Executar o código para o dataset citeseer para os valores de k = 3, 4, 5 e 6 e alg = 1, 2 e 3
+for alg in 1 2 
+do
+  for k in 3 4 5 6
+    do
+      ./main ./graph_datasets/citeseer.edgelist $k $alg >> citeseer.results
+    done
+done
 
